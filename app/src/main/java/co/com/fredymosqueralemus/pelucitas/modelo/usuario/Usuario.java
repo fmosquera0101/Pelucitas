6ec135e18b5938b467093e264061a5fbc6c4368a
Fredy Mosquera Lemus <fmosquera0101@gmail.com>
2017-02-04 09:18:34 -0500

    Se crear el modelo usuario que reprenta los usuario en la aplicacion

--- app/src/main/java/co/com/fredymosqueralemus/pelucitas/modelo/usuario/Usuario.java
+++ app/src/main/java/co/com/fredymosqueralemus/pelucitas/modelo/usuario/Usuario.java
@@ -1,5 +1,7 @@
 package co.com.fredymosqueralemus.pelucitas.modelo.usuario;
 
+import co.com.fredymosqueralemus.pelucitas.direccion.Direccion;
+
 /**
  * Esta clase representa el modelo de usuario de la aplicaion
  * Created by fredymosqueralemus on 3/02/17.
@@ -10,5 +12,54 @@ public class Usuario {
     private String nombre;
     private String apellidos;
     private String telefono;
+    private String perfil;
+    private Direccion direccion;
 
+    public String getCedulaIdentificacion() {
+        return cedulaIdentificacion;
+    }
+
+    public void setCedulaIdentificacion(String cedulaIdentificacion) {
+        this.cedulaIdentificacion = cedulaIdentificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
 }
