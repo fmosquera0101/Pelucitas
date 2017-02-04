57419f1195ce34ef617783792ef3959195fd535e
Fredy Mosquera Lemus <fredymosqueralemus@MacBook-Pro-de-Fredy.local>
2017-02-03 22:32:48 -0500

    Configuracion de la aplicacion con firebase Creacion de las vista para el registro del usuario (correo, contrasena, datos personales, direccion) Creacion de modelo usario

---
+++ app/src/main/java/co/com/fredymosqueralemus/pelucitas/modelo/usuario/Usuario.java
@@ -0,0 +1,14 @@
+package co.com.fredymosqueralemus.pelucitas.modelo.usuario;
+
+/**
+ * Esta clase representa el modelo de usuario de la aplicaion
+ * Created by fredymosqueralemus on 3/02/17.
+ */
+
+public class Usuario {
+    private String cedulaIdentificacion;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+
+}
