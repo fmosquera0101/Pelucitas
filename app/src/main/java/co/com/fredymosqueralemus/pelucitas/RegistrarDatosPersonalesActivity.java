adf459100eef29845e86ba29ccece4130283dae1
Fredy Mosquera Lemus <fmosquera0101@gmail.com>
2017-02-07 22:10:37 -0500

    Se establecio el DatePicker sobre el campo fecha de naciemiento del formulario de registro de los datos personales del usuario Se realizo la validacion en la fecha de nacimiento (Validar que la fecha no sea mayor a la fecha actual y que no sea igual a la fecha de hoy)

--- app/src/main/java/co/com/fredymosqueralemus/pelucitas/RegistrarDatosPersonalesActivity.java
+++ app/src/main/java/co/com/fredymosqueralemus/pelucitas/RegistrarDatosPersonalesActivity.java
@@ -1,11 +1,13 @@
 package co.com.fredymosqueralemus.pelucitas;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +15,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import co.com.fredymosqueralemus.pelucitas.constantes.Constantes;
@@ -30,7 +33,7 @@ public class RegistrarDatosPersonalesActivity extends AppCompatActivity {
     private EditText etxtNombre;
     private EditText etxtApellidos;
     private EditText etxtTelefono;
-
+    private EditText etxtFechaNacimiento;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     private FirebaseUser firebaseUser;
@@ -43,7 +46,8 @@ public class RegistrarDatosPersonalesActivity extends AppCompatActivity {
         etxtNombre = (EditText) findViewById(R.id.nombre_ext_registrardatospersonaleslayout);
         etxtApellidos = (EditText) findViewById(R.id.apellidos_ext_registrardatospersonaleslayout);
         etxtTelefono = (EditText) findViewById(R.id.telefono_ext_registrardatospersonaleslayout);
-
+        etxtFechaNacimiento = (EditText) findViewById(R.id.fechanacimiento_ext_registrardatospersonaleslayout);
+        mostrarDatePickerEditTextFechaNacimiento();
         mAuth = FirebaseAuth.getInstance();
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -64,7 +68,7 @@ public class RegistrarDatosPersonalesActivity extends AppCompatActivity {
             mUsuario.setNombre(etxtNombre.getText().toString().trim());
             mUsuario.setApellidos(etxtApellidos.getText().toString().trim());
             mUsuario.setTelefono(etxtTelefono.getText() != null ? etxtTelefono.getText().toString().trim() : "-");
-            mUsuario.setFechaNacimiento(UtilidadesFecha.convertirDateAString(new Date()));
+            mUsuario.setFechaNacimiento(etxtFechaNacimiento.getText().toString());
             mUsuario.setFechaInsercion(UtilidadesFecha.convertirDateAString(new Date()));
             mUsuario.setFechaModificacion(null);
 
@@ -111,7 +115,58 @@ public class RegistrarDatosPersonalesActivity extends AppCompatActivity {
             etxtTelefono.setError(getString(R.string.error_campo_requerido));
             isCamposVacio = true;
         }
+        if(TextUtils.isEmpty(etxtFechaNacimiento.getText()) ){
+            etxtFechaNacimiento.setError(getString(R.string.error_campo_requerido));
+            isCamposVacio = true;
+        }else if(!esfechaNacimientoValida(UtilidadesFecha.convertirStringADate(etxtFechaNacimiento.getText().toString()))){
+            etxtFechaNacimiento.setError(getString(R.string.error_fechanacimiento_novalida));
+            isCamposVacio = true;
+        }
 
         return isCamposVacio;
     }
+
+    /**
+     * Metodo encargado de mostrar el DatePicker en el campo EditText de la fecha de nacimiento
+     * Created by Fredy Mosquera Lemus on 7/02/17.
+     */
+    private void mostrarDatePickerEditTextFechaNacimiento(){
+        final Calendar calendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                actualizarEditTextFechaNacimiento(calendar);
+            }
+        };
+        etxtFechaNacimiento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(RegistrarDatosPersonalesActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+    private void actualizarEditTextFechaNacimiento(Calendar calendar) {
+        etxtFechaNacimiento.setText(UtilidadesFecha.convertirDateAString(calendar.getTime()));
+    }
+
+    /**
+     * Este metodo valida si la fecha de nacimiento ingresada es valida, esto es, no puede ser igual
+     * a la fecha de hoy, y no puede ser mayor a la fecha actual
+     * TODO: se requiere validar que el usuario ingresado sea mayor de edad
+     * @param fechaNacimiento
+     * @return
+     * Created by Fredy Mosquera Lemus on 7/02/17.
+     */
+    private boolean esfechaNacimientoValida(Date fechaNacimiento){
+        boolean isFechaValida = true;
+        Date fechaHoy = new Date();
+        if (fechaNacimiento.equals(UtilidadesFecha.formatearDate(fechaHoy)) || fechaNacimiento.after(fechaHoy)){
+            return false;
+        }
+        return isFechaValida;
+    }
+
 }
--- app/src/main/java/co/com/fredymosqueralemus/pelucitas/utilidades/UtilidadesFecha.java
+++ app/src/main/java/co/com/fredymosqueralemus/pelucitas/utilidades/UtilidadesFecha.java
@@ -1,5 +1,6 @@
 package co.com.fredymosqueralemus.pelucitas.utilidades;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,5 +15,26 @@ public class UtilidadesFecha {
     public static String convertirDateAString(Date dateFecha){
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constantes.FORMATO_FECHAYYYMMDD);
         return simpleDateFormat.format(dateFecha);
+    }
+    public static Date convertirStringADate(String strFecha){
+        Date fechaConvertida = null;
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constantes.FORMATO_FECHAYYYMMDD);
+            fechaConvertida =  simpleDateFormat.parse(strFecha);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return fechaConvertida;
+    }
+    public static Date formatearDate(Date fecha){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constantes.FORMATO_FECHAYYYMMDD);
+        String strFecha =  simpleDateFormat.format(fecha);
+        Date fechaConvertida = null;
+        try {
+            fechaConvertida = simpleDateFormat.parse(strFecha);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return fechaConvertida;
     }
 }
