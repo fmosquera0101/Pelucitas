05c571ae05d79ad16591e98cd14f343fd3fb9dfa
Fredy Mosquera Lemus <fmosquera0101@gmail.com>
2017-10-04 22:16:57 -0500

    Refactor de codigo BuscarEmpleadoActivity

--- app/src/main/java/co/com/fredymosqueralemus/pelucitas/BuscarEmpleadoActivity.java
+++ app/src/main/java/co/com/fredymosqueralemus/pelucitas/BuscarEmpleadoActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -65,114 +66,18 @@ public class BuscarEmpleadoActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu_search, menu);
-        MenuItem menuItem = menu.findItem(R.id.menuItemsearch);
-        SearchView searchView = (SearchView) menuItem.getActionView();
+        SearchView searchView = getSearchView(menu);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return false;
             }
-
             @Override
             public boolean onQueryTextChange(final String newText) {
                 if (!TextUtils.isEmpty(newText)) {
-                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-                    Query firebaseQuery = databaseReference.child(Constantes.USUARIO_FIREBASE_BD).orderByChild("nombre").startAt("#" + newText);
-                    firebaseQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            listUsuarios = new ArrayList<Usuario>();
-                            for (DataSnapshot child : dataSnapshot.getChildren()) {
-                                final Usuario usuario = child.getValue(Usuario.class);
-                                if (usuario.getNombre().trim().toLowerCase().contains(newText.trim().toLowerCase())) {
-                                    PerfilesXUsuario perfilesXUsuario = usuario.getPerfilEmpleado();
-                                    if ("S".equals(perfilesXUsuario.getActivo())) {
-                                        listUsuarios.add(usuario);
-                                    }
-
-                                }
-                            }
-
-                            AdapterMisEmpleados adapterMisEmpleados = new AdapterMisEmpleados(context, R.layout.layout_listview_misempleados, listUsuarios);
-                            listView.setAdapter(adapterMisEmpleados);
-
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
-
-                        }
-                    });
-                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                            builder.setMessage("Agregar Empleado");
-                            builder.setPositiveButton("Agregar", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                   final  Usuario usuario = listUsuarios.get(position);
-
-                                    DatabaseReference databaseReferenceEmpleado = FirebaseDatabase.getInstance().getReference(UtilidadesFirebaseBD.getUrlInserccionEmpleadosXNegocio(miNegocio.getNitNegocio()));
-                                    databaseReferenceEmpleado.addListenerForSingleValueEvent(new ValueEventListener() {
-                                        @Override
-                                        public void onDataChange(DataSnapshot dataSnapshot) {
-                                          boolean empleadoExisteEnNegocio = false;
-                                            for (DataSnapshot child: dataSnapshot.getChildren()) {
-                                                EmpleadosXNegocio empleadosXNegocioFromBD = child.getValue(EmpleadosXNegocio.class);
-                                                if(usuario.getUid().equals(empleadosXNegocioFromBD.getUidUsario())){
-                                                    Toast.makeText(context, "Este emplado ya esta agregado a tu negocio, intenta con otro",
-                                                            Toast.LENGTH_SHORT).show();
-                                                    empleadoExisteEnNegocio = true;
-                                                    break;
-                                                }
-                                            }
-                                            if(!empleadoExisteEnNegocio){
-
-                                                DatabaseReference databaseReferenceEmpleado = FirebaseDatabase.getInstance().getReference(UtilidadesFirebaseBD.getUrlInserccionUsuario(usuario.getUid()));
-                                                usuario.setFechaModificacion(UtilidadesFecha.convertirDateAString(new Date(), Constantes.FORMAT_DDMMYYYYHHMMSS));
-                                                usuario.setNitNegocioEmpleador(miNegocio.getNitNegocio());
-
-                                                databaseReferenceEmpleado.setValue(usuario);
-                                                databaseReferenceEmpleado = FirebaseDatabase.getInstance().getReference(UtilidadesFirebaseBD.getUrlInserccionEmpleadosXNegocio(miNegocio.getNitNegocio()));
-
-                                                EmpleadosXNegocio empleadosXNegocio = new EmpleadosXNegocio();
-                                                empleadosXNegocio.setCedulaUsuario(usuario.getCedulaIdentificacion());
-                                                empleadosXNegocio.setUidUsario(usuario.getUid());
-                                                empleadosXNegocio.setFechaInsercion(UtilidadesFecha.convertirDateAString(new Date(), Constantes.FORMAT_DDMMYYYYHHMMSS));
-
-                                                databaseReferenceEmpleado = FirebaseDatabase.getInstance().getReference(UtilidadesFirebaseBD.getUrlInserccionEmpleadosXNegocio(miNegocio.getNitNegocio()));
-                                                databaseReferenceEmpleado.push().setValue(empleadosXNegocio);
-                                                Toast.makeText(context, "Se agregó empleado a tu negocio",
-                                                        Toast.LENGTH_SHORT).show();
-                                                finish();
-                                            }
-                                        }
-
-                                        @Override
-                                        public void onCancelled(DatabaseError databaseError) {
-
-                                        }
-                                    });
-
-                                }
-                            });
-
-                            builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    getEmpleadosXNegocio();
-                                }
-                            });
-                            AlertDialog alertDialog = builder.create();
-                            alertDialog.show();
-                        }
-
-                    });
-
+                    Query firebaseQuery = getQueryFirebase(newText);
+                    consultarEmpleadosFirebase(newText, firebaseQuery);
+                    listViewSetOnItemClickListener();
                 } else {
                     listUsuarios.clear();
 
@@ -181,6 +86,142 @@ public class BuscarEmpleadoActivity extends AppCompatActivity {
             }
         });
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void consultarEmpleadosFirebase(final String newText, Query firebaseQuery) {
+        firebaseQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                addUsuarioToList(dataSnapshot, newText);
+                setAdapterMisEmpleados();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+
+            }
+        });
+    }
+
+    private Query getQueryFirebase(String newText) {
+        Query firebaseQuery;DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        firebaseQuery = databaseReference.child(Constantes.USUARIO_FIREBASE_BD).orderByChild("nombre").startAt("#" + newText);
+        return firebaseQuery;
+    }
+
+    private void addUsuarioToList(DataSnapshot dataSnapshot, String newText) {
+        listUsuarios = new ArrayList<Usuario>();
+        for (DataSnapshot child : dataSnapshot.getChildren()) {
+            final Usuario usuario = child.getValue(Usuario.class);
+            if (usuario.getNombre().trim().toLowerCase().contains(newText.trim().toLowerCase())) {
+                PerfilesXUsuario perfilesXUsuario = usuario.getPerfilEmpleado();
+                if ("S".equals(perfilesXUsuario.getActivo())) {
+                    listUsuarios.add(usuario);
+                }
+
+            }
+        }
+    }
+
+    private void listViewSetOnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage(getString(R.string.str_agregarempleado));
+                createBuilderPositiveNegativeButtons(position, builder);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+
+        });
+    }
+
+    private void createBuilderPositiveNegativeButtons(final int position, AlertDialog.Builder builder) {
+        builder.setPositiveButton(getString(R.string.str_agregar), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Usuario usuario = listUsuarios.get(position);
+                consultarInformacionEmpleadoFirebase(usuario);
+
+            }
+        });
+
+        builder.setNegativeButton(getString(R.string.str_cancelar), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                getEmpleadosXNegocio();
+            }
+        });
+    }
+
+    private void consultarInformacionEmpleadoFirebase(final Usuario usuario) {
+        DatabaseReference databaseReferenceEmpleado = FirebaseDatabase.getInstance().getReference(UtilidadesFirebaseBD.getUrlInserccionEmpleadosXNegocio(miNegocio.getNitNegocio()));
+        databaseReferenceEmpleado.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean empleadoExisteEnNegocio = isEmpleadoExisteEnNegocio(dataSnapshot, usuario);
+                if(!empleadoExisteEnNegocio){
+                    usuario.setFechaModificacion(getFechaModificacion());
+                    usuario.setNitNegocioEmpleador(miNegocio.getNitNegocio());
+                    actualizarInfoUsuarioFirebase(usuario);
+                    EmpleadosXNegocio empleadosXNegocio = getEmpleadosXNegocio(usuario);
+                    insertarEmpleadoXNegocioFirebase(empleadosXNegocio);
+                    Toast.makeText(context, getString(R.string.str_msj_empleado_agregado), Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private boolean isEmpleadoExisteEnNegocio(DataSnapshot dataSnapshot, Usuario usuario) {
+        boolean empleadoExisteEnNegocio = false;
+        for (DataSnapshot child: dataSnapshot.getChildren()) {
+            EmpleadosXNegocio empleadosXNegocioFromBD = child.getValue(EmpleadosXNegocio.class);
+            if(usuario.getUid().equals(empleadosXNegocioFromBD.getUidUsario())){
+                Toast.makeText(context, getString(R.string.str_msj_empleado_ya_agregado), Toast.LENGTH_SHORT).show();
+                empleadoExisteEnNegocio = true;
+                break;
+            }
+        }
+        return empleadoExisteEnNegocio;
+    }
+
+    private void actualizarInfoUsuarioFirebase(Usuario usuario) {
+        DatabaseReference databaseReferenceEmpleado = FirebaseDatabase.getInstance().getReference(UtilidadesFirebaseBD.getUrlInserccionUsuario(usuario.getUid()));
+        databaseReferenceEmpleado.setValue(usuario);
+    }
+
+    private void insertarEmpleadoXNegocioFirebase(EmpleadosXNegocio empleadosXNegocio) {
+        DatabaseReference databaseReferenceEmpleado;
+        databaseReferenceEmpleado = FirebaseDatabase.getInstance().getReference(UtilidadesFirebaseBD.getUrlInserccionEmpleadosXNegocio(miNegocio.getNitNegocio()));
+        databaseReferenceEmpleado.push().setValue(empleadosXNegocio);
+    }
+
+    @NonNull
+    private EmpleadosXNegocio getEmpleadosXNegocio(Usuario usuario) {
+        EmpleadosXNegocio empleadosXNegocio = new EmpleadosXNegocio();
+        empleadosXNegocio.setCedulaUsuario(usuario.getCedulaIdentificacion());
+        empleadosXNegocio.setUidUsario(usuario.getUid());
+        empleadosXNegocio.setFechaInsercion(getFechaModificacion());
+        return empleadosXNegocio;
+    }
+
+    private String getFechaModificacion() {
+        return UtilidadesFecha.convertirDateAString(new Date(), Constantes.FORMAT_DDMMYYYYHHMMSS);
+    }
+
+    private SearchView getSearchView(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_search, menu);
+        MenuItem menuItem = menu.findItem(R.id.menuItemsearch);
+        return (SearchView) menuItem.getActionView();
     }
 
     private void getEmpleadosXNegocio(){
@@ -197,8 +238,7 @@ public class BuscarEmpleadoActivity extends AppCompatActivity {
 
 
                 }
-                AdapterMisEmpleados adapterMisEmpleados = new AdapterMisEmpleados(context, R.layout.layout_listview_misempleados, listUsuarios);
-                listView.setAdapter(adapterMisEmpleados);
+                setAdapterMisEmpleados();
 
             }
 
@@ -207,5 +247,10 @@ public class BuscarEmpleadoActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void setAdapterMisEmpleados() {
+        AdapterMisEmpleados adapterMisEmpleados = new AdapterMisEmpleados(context, R.layout.layout_listview_misempleados, listUsuarios);
+        listView.setAdapter(adapterMisEmpleados);
     }
 }
